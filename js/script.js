01faea879bb74e09ca6841c21a23f71ceddb90ea
sexfat <bryant6831@gmail.com>
2025-02-22 17:46:01 +0800

    add include header

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("../header.html")
+        .then(response => response.text())  // 讀取 HTML 內容
+        .then(data => {
+            document.getElementById("header-container").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading header:", error));
+});
